7f86956f6e34f5760317ab41e633a68ebe432f5f
jakubkucharski <jakubkucharski@p1-im.co.uk>
2025-01-04 19:53:04 +0100

    fixed issues in globalUmr sp

--- export.globalUmrFilter.sql
+++ export.globalUmrFilter.sql
@@ -10,7 +10,9 @@ create or alter procedure [export].[sp_GlobalUmrFilter]
     ,@search varchar(50)
     ,@sort varchar(50)
 as
+
 begin
+
 declare @sql nvarchar(max)
 declare @v_umr_criteria varchar(20)
 declare @v_fcp_status varchar(20)
@@ -18,6 +20,9 @@ declare @v_reserve varchar(20)
 declare @v_claim_status varchar(20)
 declare @v_pageOffset varchar(5) = (select cast(@pageSize*(@page - 1) as varchar(10)))
 declare @v_pageSize varchar(5) = (select cast(@pageSize as varchar(10)))
+
+declare @newId varchar(50) = (select replace(newid(),'-',''))
+
 set @v_umr_criteria = case @umr_criteria
                         when 'Active' then '1'
                         when 'Inactive' then '0'
@@ -39,21 +44,26 @@ set @v_claim_status = case @claim_status
                     when 'False' then '0'
                     when 'All' then '0,1'
                  end
+
+
 ---- Role handling -----
-drop table if exists ##roleValues 
+drop table if exists #roleValues 
+
 select
     [value]
-into ##roleValues
+into #roleValues
 from string_split(@role_value, ',')
+
 declare @roleValueWhereClause varchar(255) = ''
 declare @roleUmrJoin varchar(255) = ''
+
 if @role <> 'admin'
 begin
-    drop table if exists ##roleFiltered
+    drop table if exists #roleFiltered
     
     set @sql = 'select '
         + @role + '
-    into ##roleFiltered
+    into #roleFiltered
     from (
         select 
             t1.[' + @role + ']
@@ -61,16 +71,22 @@ begin
         from [rls_filterset_umr_' + @role + '] umr
         join [rls_filterset_rls_' + @role + '] t1 on umr.[RLS_' + @role + '_id] = t1.[id]
         join [rls_filterset_rls_' + @role + '] t2 on t1.[parent_id] = t2.[id]
-        where t2.[' + @role + '] in (select [value] from ##roleValues)
+        where t2.[' + @role + '] in (select [value] from #roleValues)
     ) w
     where [rowN] = 1'
     
     exec(@sql)
-    set @roleValueWhereClause = ' where d.[' + @role + '_Name] in (select ' + @role + ' from ##roleFiltered)'
-    drop table if exists ##roleUmrFiltered
+
+    set @roleValueWhereClause = ' where d.[' + @role + '_Name] in (select ' + @role + ' from #roleFiltered)'
+
+	declare @t_roleUmrFiltered varchar(75) = '##roleUmrFiltered' + @newId
+	set @sql = 'drop table if exists ' + @t_roleUmrFiltered
+
+    exec(@sql)
+
     set @sql = 'select 
         [UMR_Risk_Section]
-    into ##roleUmrFiltered
+    into ' + @t_roleUmrFiltered + '
     from (
         select 
             umr.[UMR_Risk_Section]
@@ -78,14 +94,20 @@ begin
         from [rls_filterset_umr_' + @role + '] umr
         join [rls_filterset_rls_' + @role + '] t1 on umr.[RLS_' + @role + '_id] = t1.[id]
         join [rls_filterset_rls_' + @role + '] t2 on t1.[parent_id] = t2.[id]
-        where t2.[' + @role + '] in (select [value] from ##roleValues)
+        where t2.[' + @role + '] in (select [value] from #roleValues)
     ) w
     where [rowN] = 1'
     
     exec(@sql)
-    set @roleUmrJoin = 'join ##roleUmrFiltered r on r.[UMR_Risk_Section] = d.[UMR_Risk_Section]'
+    set @roleUmrJoin = 'join ' + @t_roleUmrFiltered + ' r on r.[UMR_Risk_Section] = d.[UMR_Risk_Section]'
 end
-if object_id('tempdb..##dataRows') is not null drop table ##dataRows
+
+
+declare @t_dataRows varchar(75) = '##dataRows' + @newId
+set @sql = 'drop table if exists ' + @t_dataRows
+
+exec(@sql)
+
 set @sql = 'select distinct
     d.[UMR_Risk_Cat_Section]
    ,case
@@ -102,12 +124,17 @@ set @sql = 'select distinct
         when [Claim_Status] in (''c'', ''cl'', ''clre'', ''closed'', ''re-closed'', ''withdrawn'', ''denied'') then 0
         else 1
      end as [ClaimStatusFlag]
-into ##dataRows
+into ' + @t_dataRows + '
 from [dbo].[rig_datarows] d
-'
-+ @roleUmrJoin
-if object_id('tempdb..##tempGlobalUmr') is not null drop table ##tempGlobalUmr
+' + @roleUmrJoin
+
 exec sp_executesql @sql
+
+declare @t_tempGlobalUmr varchar(75) = '##tempGlobalUmr' + @newId
+set @sql = 'drop table if exists ' + @t_tempGlobalUmr
+
+exec(@sql)
+
 set @sql = 'select
         [id]
         ,[created_on]
@@ -126,7 +153,7 @@ set @sql = 'select
         ,[reserve_status]
         ,[claim_status]
         --,row_number() over(order by [umr],[created_on],[umr]) as [rowN]
-    into ##tempGlobalUmr
+    into ' + @t_tempGlobalUmr + '
     from (
         select 
             g.[id]
@@ -147,7 +174,7 @@ set @sql = 'select
             ,d.[ClaimStatusFlag] as [claim_status]
             ,row_number() over(partition by g.[id] order by g.[id]) as [rowN]
         from [dbo].[rls_filterset_globalumr] g
-        join ##dataRows d on d.[UMR_Risk_Cat_Section] = g.[umr_rc_cc_sn]
+        join ' + @t_dataRows + ' d on d.[UMR_Risk_Cat_Section] = g.[umr_rc_cc_sn]
         where g.[bdx_status] in (' + @v_umr_criteria + ') 
             and g.[fcp_status] in (' + @v_fcp_status + ') 
             and d.[ClaimStatusFlag] in (' + @v_claim_status + ') 
@@ -158,9 +185,32 @@ set @sql = 'select
                 or g.[cat_code] like ''%' + @search + '%'')
     ) w
     where [rowN] = 1'
+
 exec sp_executesql @sql
-if object_id('tempdb..##finalResult') is not null drop table ##finalResult
+
+drop table if exists #finalResult
+
+create table #finalResult (
+     [id] int null
+    ,[created_on] datetime2(7) null
+    ,[modified_on] datetime2(7) null
+    ,[umr] nvarchar(128)
+    ,[bdx_status] bit null
+    ,[fcp_status] bit null
+    ,[risk_code] nvarchar(64) null
+    ,[section_no] nvarchar(64) null
+    ,[cat_code] nvarchar(64) null
+    ,[umr_rc_cc_sn] nvarchar(350) null
+    ,[umr_rc_sn] nvarchar(256) null
+    ,[billing_status] bit null
+    ,[currency_type] nvarchar(50) null
+    ,[fcp_start_date] date null
+    ,[reserve_status] bit null
+    ,[claim_status]	bit null
+)
+
 set @sql = '
+insert #finalResult ([id],[created_on],[modified_on],[umr],[bdx_status],[fcp_status],[risk_code],[section_no],[cat_code],[umr_rc_cc_sn],[umr_rc_sn],[billing_status],[currency_type],[fcp_start_date],[reserve_status],[claim_status])
 select
     [id]
     ,[created_on]
@@ -178,12 +228,20 @@ select
     ,[fcp_start_date]
     ,[reserve_status]
     ,[claim_status]
-into  ##finalResult
-from ##tempGlobalUmr
+from ' + @t_tempGlobalUmr + '
 order by ' + @sort + '
 offset ' + @v_pageOffset + ' rows
 fetch next ' + @v_pageSize + ' rows only'
+
 exec sp_executesql @sql
-select count(*) as TotalRecords from ##tempGlobalUmr 
-select * from  ##finalResult
+
+drop table if exists #count
+create table #count (
+	[TotalRecords] int null
+)
+set @sql = 'insert #count ([TotalRecords]) select count(*) from ' + @t_tempGlobalUmr
+exec(@sql)
+
+select [TotalRecords] from #count
+select * from  #finalResult
 end
